c33056d38f8f779a47075cab6f1779dc68b871aa
WazowM <106464087+WazowM@users.noreply.github.com>
2022-05-29 21:05:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,66 @@
 const symbol = require('/node_modules/symbol-sdk')
 
+//Symbolテストネットで使用する設定値
 const GENERATION_HASH = '7FCCD304802016BEBBCD342A332F91FF1F3BB5E902988B352697BE245F48E836'
 const EPOCH = 1637848847
 const XYM_ID = '3A8416DB2D53B6C8'
 const NODE_URL = 'https://sym-test.opening-line.jp:3001'
 const NET_TYPE = symbol.NetworkType.TEST_NET
+
+//XYM所持量表示
+const repositoryFactory = new symbol.RepositoryFactoryHttp(NODE_URL)
+const accountHttp = repositoryFactory.createAccountRepository()
+
+accountHttp.getAccountInfo(address)
+  .toPromise()
+  .then((accountInfo) => {
+    for (let m of accountInfo.mosaics) {
+      if (m.id.id.toHex() === XYM_ID) {
+        const dom_xym = document.getElementById('wallet-xym')
+        dom_xym.innerText = `XYM Balance : ${m.amount.compact() / Math.pow(10, 6)}`
+      }
+    }
+  })
+
+//トランザクションの履歴取得
+const transactionHttp = repositoryFactory.createTransactionRepository()
+const searchCriteria = {
+  group: symbol.TransactionGroup.Confirmed,
+  address,
+  pageNumber: 1,
+  pageSize: 20,
+  order: symbol.Order.Desc,
+}
+
+
+//トランザクションリストを作ら目の要素をついか
+transactionHttp
+  .search(searchCriteria)
+  .toPromise()
+  .then((txs) => {
+    console.log(txs)
+    const dom_txInfo = document.getElementById('wallet-transactions')
+    for (let tx of txs.data) {
+      console.log(tx)
+      const dom_tx = document.createElement('div')
+      const dom_txType = document.createElement('div')
+      const dom_hash = document.createElement('div')
+
+      dom_txType.innerText = `Transaction Type : ${getTransactionType(tx.type)}`
+      dom_hash.innerText = `Transaction Hash : ${tx.transactionInfo.hash}`
+
+      dom_tx.appendChild(dom_txType)
+      dom_tx.appendChild(dom_hash)
+      dom_tx.appendChild(document.createElement('hr'))
+
+      dom_txInfo.appendChild(dom_tx)
+    }
+  })
+
+function getTransactionType (type) {
+  if (type === 16724) return 'TRANSFER TRANSACTION'
+  return 'OTHER TRANSACTION'
+}
 
 const address = symbol.Address.createFromRawAddress("TDB6EPSUXT6RFLQMJDWPVO5E7TJN2AW3KLEEO6A")
 
