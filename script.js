9aaeed0acdbf0585e1b4c8f05b429c4b0892c4f4
WazowM <106464087+WazowM@users.noreply.github.com>
2022-05-29 21:47:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ const EPOCH = 1637848847
 const XYM_ID = '3A8416DB2D53B6C8'
 const NODE_URL = 'https://sym-test.opening-line.jp:3001'
 const NET_TYPE = symbol.NetworkType.TEST_NET
+
+//XYM保持量表示
+const repositoryFactory = new symbol.RepositoryFactoryHttp(NODE_URL)
+const accountHttp = repositoryFactory.createAccountRepository()
+
+accountHttp.getAccountInfo(address)
+  .toPromise()
+  .then((accountInfo) => {
+    for (let m of accountInfo.mosaics) {
+      if (m.id.id.toHex() === XYM_ID) {
+        const dom_xym = document.getElementById('wallet-xym')
+        dom_xym.innerText = `XYM Balance : ${m.amount.compact() / Math.pow(10, 6)}`
+      }
+    }
+  })
+
+//トランザクション履歴
+const transactionHttp = repositoryFactory.createTransactionRepository()
+const searchCriteria = {
+  group: symbol.TransactionGroup.Confirmed,
+  address,
+  pageNumber: 1,
+  pageSize: 20,
+  order: symbol.Order.Desc,
+}
