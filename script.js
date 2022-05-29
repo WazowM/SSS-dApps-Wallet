9b067efe1aa5ef24c92bab5a35be1874fe9aa775
WazowM <106464087+WazowM@users.noreply.github.com>
2022-05-29 18:31:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const symbol = require('/node_modules/symbol-sdk')
 
-console.log("Hello Symbol")
+const address = symbol.Address.createFromRawAddress("TDB6EPSUXT6RFLQMJDWPVO5E7TJN2AW3KLEEO6A")
+console.log("Oh Yeah!! Symbol")
+console.log(`Your Address : ${address.plain()}`)
