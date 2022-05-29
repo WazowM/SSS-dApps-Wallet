b21475cdc56dd3a76c6bac3c68466d5f7c81ec21
WazowM <106464087+WazowM@users.noreply.github.com>
2022-05-29 20:22:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const XYM_ID = '3A8416DB2D53B6C8'
 const NODE_URL = 'https://sym-test.opening-line.jp:3001'
 const NET_TYPE = symbol.NetworkType.TEST_NET
 
-const address = symbol.Address.createFromRawAddress("TAD7Q3FEN5CZRZFE3WX6TWEESATVTMJDS2ETVTY")
+const address = symbol.Address.createFromRawAddress("TDB6EPSUXT6RFLQMJDWPVO5E7TJN2AW3KLEEO6A")
 
 console.log("Hello Symbol")
 console.log(`Your Address : ${address.plain()}`)
@@ -91,3 +91,111 @@ function handleClick() {
 
   transactionHttp.announce(signedTx)
 }
+
+//トランザクションを生成
+  const tx = symbol.TransferTransaction.create(
+    symbol.Deadline.create(EPOCH),
+    symbol.Address.createFromRawAddress(addr),
+    [
+      new symbol.Mosaic(
+        new symbol.MosaicId(XYM_ID),
+        symbol.UInt64.fromUint(Number(amount))
+      )
+    ],
+    symbol.PlainMessage.create(message),
+    NET_TYPE,
+    symbol.UInt64.fromUint(2000000)
+  )
+  
+  //署名
+  const pk = 'CAB93A8E8966A5DAD9D9956DABEEE184624AD7BBE3EAEF469871563322E8D6CB'
+
+  const acc = symbol.Account.createFromPrivateKey(pk, NET_TYPE)
+  
+  const signedTx = acc.sign(tx, GENERATION_HASH)
+  
+  //アナウンス
+  transactionHttp.announce(signedTx)
+
+  //handleSSS関数
+function handleSSS() {
+  console.log('handle sss')
+  const addr = document.getElementById('form-addr').value
+  const amount = document.getElementById('form-amount').value
+  const message = document.getElementById('form-message').value
+  
+  const tx = symbol.TransferTransaction.create(
+    symbol.Deadline.create(EPOCH),
+    symbol.Address.createFromRawAddress(addr),
+    [
+      new symbol.Mosaic(
+        new symbol.MosaicId(XYM_ID),
+        symbol.UInt64.fromUint(Number(amount))
+      )
+    ],
+    symbol.PlainMessage.create(message),
+    NET_TYPE,
+    symbol.UInt64.fromUint(2000000)
+  )
+
+  window.SSS.setTransaction(tx)
+
+  window.SSS.requestSign().then(signedTx => {
+    console.log('signedTx', signedTx)
+    transactionHttp.announce(signedTx)
+  })
+}
+
+//使用アドレス取得
+const repositoryFactory = new symbol.RepositoryFactoryHttp(NODE_URL)
+const accountHttp = repositoryFactory.createAccountRepository()
+const transactionHttp = repositoryFactory.createTransactionRepository()
+
+setTimeout(() => {
+  
+const address = symbol.Address.createFromRawAddress(window.SSS.activeAddress)
+
+const dom_addr = document.getElementById('wallet-addr')
+dom_addr.innerText = address.pretty()
+
+accountHttp.getAccountInfo(address)
+  .toPromise()
+  .then((accountInfo) => {
+    for (let m of accountInfo.mosaics) {
+      if (m.id.id.toHex() === XYM_ID) {
+        const dom_xym = document.getElementById('wallet-xym')
+        dom_xym.innerText = `XYM Balance : ${m.amount.compact() / Math.pow(10, 6)}`
+      }
+    }
+  })
+const searchCriteria = {
+  group: symbol.TransactionGroup.Confirmed,
+  address,
+  pageNumber: 1,
+  pageSize: 20,
+  order: symbol.Order.Desc,
+}
+
+transactionHttp
+  .search(searchCriteria)
+  .toPromise()
+  .then((txs) => {
+    console.log(txs)
+    const dom_txInfo = document.getElementById('wallet-transactions')
+    for (let tx of txs.data) {
+      console.log(tx)
+      const dom_tx = document.createElement('div')
+      const dom_txType = document.createElement('div')
+      const dom_hash = document.createElement('div')
+
+      dom_txType.innerText = `Transaction Type : ${getTransactionType(tx.type)}`
+      dom_hash.innerText = `Transaction Hash : ${tx.transactionInfo.hash}`
+
+      dom_tx.appendChild(dom_txType)
+      dom_tx.appendChild(dom_hash)
+      dom_tx.appendChild(document.createElement('hr'))
+
+      dom_txInfo.appendChild(dom_tx)
+    }
+  })
+}, 500)
