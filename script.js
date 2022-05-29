a3de7c2e48be40278478361e978d5e6a6d49f023
WazowM <106464087+WazowM@users.noreply.github.com>
2022-05-29 21:39:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const symbol = require('/node_modules/symbol-sdk')
 
+//コンソールに表示
 const address = symbol.Address.createFromRawAddress("TAD7Q3FEN5CZRZFE3WX6TWEESATVTMJDS2ETVTY")
 console.log("Hello Symbol")
 console.log(`Your Address : ${address.plain()}`)
+
+//アドレス表示
+const dom_addr = document.getElementById('wallet-addr')
+dom_addr.innerText = address.pretty()
